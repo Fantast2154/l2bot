1ce19e469a5ae84066ee9cadc846b98d5c8525d1
Fantast2154 <spectra.robots@gmail.com>
2021-04-04 20:01:32 +0200

    solved the problem with relative import

--- fisher/action_service.py
+++ fisher/action_service.py
@@ -2,7 +2,7 @@ class ActionService:
     def __init__(self):
         pass
 
-    def mouse_click(self, point, click=True, button='LEFT', slow=False): #add dable-click
+    def mouse_click(self, point, click=True, button='LEFT', slow=False): #add da
         pass
 
     def keyboard_press_hold_release(self, hold_time):
--- fisher/fishing_window.py
+++ fisher/fishing_window.py
@@ -1,5 +1,8 @@
-# import screen_analyzer
-from fisher.screen_analyzer import ScreenAnalyzer
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# import ../db.py
+# from fisher.screen_analyzer import ScreenAnalyzer
+import screen_analyzer
 
 class FishingWindow:
     def __init__(self, left_top_x, left_top_y, right_bottom_x, right_bottom_y):
