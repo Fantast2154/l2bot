098a97457756c26d66b830e0c50ff3b05eb9f339
MedwedHub <medwed.sib@gmail.com>
2021-04-24 01:08:20 +0700

    Запусти ЛЮБОЕ количество окон ЛА2 и запусти test_multi_window_capture.py Наслаждайся.

--- tests/test_multi_window_capture.py
+++ tests/test_multi_window_capture.py
@@ -1,11 +1,13 @@
 from system.window_capture import WindowCapture
 import cv2
+import keyboard
+
 
 wincap = WindowCapture('Asterios')
-wincap.capture_screen()
 
-for i, img in enumerate(wincap.imgs):
-    print(len(wincap.imgs))
-    cv2.imshow(f'{i}', img)
-
-cv2.waitKey(0)
+while not keyboard.is_pressed('q'):
+    wincap.capture_screen()
+    for i, img in enumerate(wincap.imgs):
+        #print(len(wincap.imgs))
+        cv2.imshow(f'{i}', img)
+        cv2.waitKey(1)
