236c8d6d892585eacd05382b4f8feefe12673ed5
MedwedHub <medwed.sib@gmail.com>
2021-08-29 02:26:30 +0700

    Автоматический горячий пирог

--- system/cpd.py
+++ system/cpd.py
@@ -1,15 +1,15 @@
 class CPD:
     number_of_windows = 3
     accounts = {
-        'podscarbiy': ['gnossienne', 'usUi2001'],
-        'starosta_derevni': ['nFantast2151', 'AA5931593aa'],
-        'nat_peggl': ['nFantast2152', 'AA5931593aa']
+        'boyko1': ['privetvsem2', 'hpzaiCKwmFpX2'],
+        'boyko2': ['smeshnoy', 'Wcg-rtT-2i9-rxY'],
+        'boyko3': ['privetvsem', 'Km0G46x18YL5']
     }
     relog_logins = []
     relog_passwords = []
     # logins = ['gnossienne', 'nFantast2151', 'nFantast2152']
     # passwords = ['usUi2001', 'AA5931593aa', 'AA5931593aa']
-    launcher_path = r'C:/Users/Dmitry/Desktop/LineageII @ Asterios'
+    launcher_path = r'E:\TorrentsDownloads\god_tauti_asterios\Asterios1.exe.lnk'
 
     def __init__(self):
         pass
