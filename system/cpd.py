9facef5e8229ffcaaa0c9f7fef7c9c8aec6453c1
Fantast2154 <spectra.robots@gmail.com>
2021-08-28 21:45:35 +0200

    1

--- system/cpd.py
+++ system/cpd.py
@@ -1,15 +1,15 @@
 class CPD:
     number_of_windows = 3
     accounts = {
-        'boyko1': ['privetvsem2', 'hpzaiCKwmFpX2'],
-        'boyko2': ['smeshnoy', 'Wcg-rtT-2i9-rxY'],
-        'boyko3': ['privetvsem', 'Km0G46x18YL5']
+        'podscarbiy': ['log1', 'pas1'],
+        'starosta_derevni': ['log2', 'pas2'],
+        'nat_peggl': ['log3', 'pas3']
     }
     relog_logins = []
     relog_passwords = []
     # logins = ['gnossienne', 'nFantast2151', 'nFantast2152']
     # passwords = ['usUi2001', 'AA5931593aa', 'AA5931593aa']
-    launcher_path = r'E:\TorrentsDownloads\god_tauti_asterios\Asterios1.exe.lnk'
+    launcher_path = r'path'
 
     def __init__(self):
         pass
